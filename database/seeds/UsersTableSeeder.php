b400bb59091dc4e0122c0d3b347c9f955d6b1754
Favor <favoriabs@gmail.com>
2017-11-24 08:11:05 +0100

    seeds

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,5 +16,83 @@ class UsersTableSeeder extends Seeder
       'email' => 'info@cwcci.com',
       'password' => bcrypt('cwcci@123'),
       ]);
+
+      DB::table('users')->insert([
+      'name' => 'CWCCI 2',
+      'email' => 'info2@cwcci.com',
+      'password' => bcrypt('cwcci@123'),
+      ]);
+
+      DB::table('users')->insert([
+      'name' => 'CWCCI 3',
+      'email' => 'info3@cwcci.com',
+      'password' => bcrypt('cwcci@123'),
+      ]);
+
+      DB::table('users')->insert([
+      'name' => 'CWCCI 4',
+      'email' => 'info4@cwcci.com',
+      'password' => bcrypt('cwcci@123'),
+      ]);
+
+      DB::table('users')->insert([
+      'name' => 'CWCCI 5',
+      'email' => 'info5@cwcci.com',
+      'password' => bcrypt('cwcci@123'),
+      ]);
+
+      DB::table('users')->insert([
+      'name' => 'CWCCI 6',
+      'email' => 'info6@cwcci.com',
+      'password' => bcrypt('cwcci@123'),
+      ]);
+
+      DB::table('users')->insert([
+      'name' => 'CWCCI 7',
+      'email' => 'info7@cwcci.com',
+      'password' => bcrypt('cwcci@123'),
+      ]);
+
+      DB::table('users')->insert([
+      'name' => 'CWCCI 8',
+      'email' => 'info8@cwcci.com',
+      'password' => bcrypt('cwcci@123'),
+      ]);
+
+      DB::table('users')->insert([
+      'name' => 'CWCCI 9',
+      'email' => 'info9@cwcci.com',
+      'password' => bcrypt('cwcci@123'),
+      ]);
+
+      DB::table('users')->insert([
+      'name' => 'CWCCI 10',
+      'email' => 'info10@cwcci.com',
+      'password' => bcrypt('cwcci@123'),
+      ]);
+
+      DB::table('users')->insert([
+      'name' => 'CWCCI 11',
+      'email' => 'info11@cwcci.com',
+      'password' => bcrypt('cwcci@123'),
+      ]);
+
+      DB::table('users')->insert([
+      'name' => 'CWCCI 12',
+      'email' => 'info12@cwcci.com',
+      'password' => bcrypt('cwcci@123'),
+      ]);
+
+      DB::table('users')->insert([
+      'name' => 'CWCCI 13',
+      'email' => 'info13@cwcci.com',
+      'password' => bcrypt('cwcci@123'),
+      ]);
+
+      DB::table('users')->insert([
+      'name' => 'CWCCI 14',
+      'email' => 'info14@cwcci.com',
+      'password' => bcrypt('cwcci@123'),
+      ]);
     }
 }
