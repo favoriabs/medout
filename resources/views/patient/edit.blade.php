8fd389f3314c8570d97b50b90adb862010f9b0cd
Favor <favoriabs@gmail.com>
2017-11-25 07:38:32 +0100

    correct values for drop down

--- resources/views/patient/edit.blade.php
+++ resources/views/patient/edit.blade.php
@@ -80,10 +80,10 @@
                                       <option value="rayfield" @if($patient->address_category == 'rayfield') selected @endif>Rayfield</option>
                                       <option value="vitafoam" @if($patient->address_category == 'vitafoam') selected @endif>Vitafoam</option>
                                       <option value="hwolshe" @if($patient->address_category == 'hwolshe') selected @endif>Hwolshe</option>
-                                      <option value="domkatbali" @if($patient->address_category == 'domkatbali') selected @endif>Domkat Bali</option>
-                                      <option value="millqtrs" @if($patient->address_category == 'millqtrs') selected @endif>Mill Qtrs</option>
+                                      <option value="domkatbali" @if($patient->address_category == 'domkat bali') selected @endif>Domkat Bali</option>
+                                      <option value="millqtrs" @if($patient->address_category == 'mill qtrs') selected @endif>Mill Qtrs</option>
                                       <option value="abattoir" @if($patient->address_category == 'abattoir') selected @endif>Abattoir</option>
-                                      <option value="tudunwada" @if($patient->address_category == 'tudunwada') selected @endif>Tudun Wada</option>
+                                      <option value="tudunwada" @if($patient->address_category == 'tudun wada') selected @endif>Tudun Wada</option>
                                       <option value="others" @if($patient->address_category == 'others') selected @endif>Others</option>
                                     </select>
                                 </div>
